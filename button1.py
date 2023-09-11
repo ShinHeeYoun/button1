cabcfcf6995612599bf211e49288113b713c1ca6
ShinHeeYoun <eustaph7725@gmail.com>
2023-09-11 10:56:32 +0900

    put my name&num

--- button1.py
+++ button1.py
@@ -2,10 +2,24 @@ from tkinter import *
 
 window =Tk()
 
-b1 = Button(window, text="Yeon Ryu")
-b2 = Button(window, text="60207777")
-b1.grid(row=0, column=0)
-b2.grid(row=1, column=1)
+# 공백 넣기
+bl1 = Label(window, text=" ")
+bl1.grid(row=1, column=0)  
+bl2 = Label(window, text=" ")
+bl2.grid(row=1, column=2)  
+bl3 = Label(window, text=" ")
+bl3.grid(row=1, column=4)  
+bl4 = Label(window, text=" ")
+bl4.grid(row=0, column=0) 
+bl5 = Label(window, text=" ")
+bl5.grid(row=2, column=4) 
+
+# 문자 넣기
+b1 = Button(window, text="Shin heeyoun")
+b2 = Button(window, text="60172166")
+# 위치 고정
+b1.grid(row=1, column=1)
+b2.grid(row=1, column=3)
 
 window.mainloop()
 
